d8974cdf86fb1f05045e736387fa33fe7719c7c5
Rhaim <rhaimeliasquevici@yahoo.com.br>
2021-07-27 01:46:50 +0100

    feature: adicionando outras mensagens ao acesso ao servidor 2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain;charset=utf-8'
-  })
-  res.end('Servidor funcionando redondinho, ó!')
-}).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
+    res.writeHead(200, {
+        'Content-Type': 'text/html;charset=utf-8'
+      })
+      switch (req.url) {
+        case '/':
+          return res.end('<h1>VOCÊ ESTÁ NA HOME</h1>')
+        case '/sobre':
+          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+        case '/produto':
+          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
+        default:
+          return res.end(`<h1>NÃO EXISTE ESSA PÁGINA${req.url.toUpperCase().replace(/\/|-/g,' ')}</h1>`)
+      }
+    })
