ee84b5c8563c499b07707af8c0808797131e732b
Rhaim <rhaimeliasquevici@yahoo.com.br>
2021-07-27 01:24:22 +0100

    feature: adicionando outro texto e utf-8

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const http = require('http')
 
-http.createServer(
-    (req, res) => {
-
-            res.end('Servidor funcionando')
-
-    }
-    ).listen(3000, 'localhost', () => console.log('O servidor está rodando!'))
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain;charset=utf-8'
+  })
+  res.end('Servidor funcionando redondinho, ó!')
+}).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
