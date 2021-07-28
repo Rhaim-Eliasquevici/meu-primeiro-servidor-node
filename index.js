5b052dba19e7faf522ba73255f7d90d3ae9dcb76
Rhaim <rhaimeliasquevici@yahoo.com.br>
2021-07-29 00:20:09 +0100

    feature: instalando o nodemon como dev dependency

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   app = express()
 
   app.get('/', (req, res) => {
-    res.send('<h1>VOCÊ ESTÁ NA HOME</h1>')
+    res.send('<h1>VOCÊ ESTÁ NA HOMEPAGE</h1>')
   })
 
 app.listen(3000, () => console.log('O servidor está rodando...'))
